309ec4a2ef029f71a59727f52f8eaf97bca31688
Himanshu <mehra.himanshu1708@gmail.com>
2026-05-03 21:34:37 +0530

    blogs app and blog model

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 
 # done by me not already there
 from . import views 
+from django.conf.urls.static import static  
+from django.conf import settings 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.home , name='home'),
-]
+] + static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT) 
